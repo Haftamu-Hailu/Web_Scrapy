74dd7016db0c4b670d564553bf51aa9fddcc3356
akashjorss <akash.jorss@gmail.com>
2020-04-17 01:51:08 +0200

    Added elasticsearch connector to spider

--- imdb/imdb/spiders/actors_info.py
+++ imdb/imdb/spiders/actors_info.py
@@ -2,7 +2,19 @@
 import scrapy
 import unidecode
 import re
-import random
+from elasticsearch import Elasticsearch
+from ssl import create_default_context
+import certifi
+import uuid
+
+context = create_default_context(cafile=certifi.where())
+es = Elasticsearch(
+    cloud_id="actors_db:dXMtZWFzdC0xLmF3cy5mb3VuZC5pbyQzYzFjNjZkN2Q4ZDk0NjkzYjg1YmRlNzhhY2I0ZjA0YyQzZWZmM2EyYWZiYTg0OWYyYTk4YzUzOGExNDU0NGY4ZQ==",
+    http_auth=('elastic', '9YEFAqvb3aEQ2boBGeH9Ilda'),
+    scheme="https",
+    port=9243,
+    ssl_context=context,
+)
 #remove all leading, trailing spaces, newline and brackets
 cleanString = lambda x: '' if x is None else unidecode.unidecode(re.sub(r'^\s+|\s+$|\n|\(|\)','',x))
 
@@ -16,6 +28,8 @@ class ActorsInfoSpider(scrapy.Spider):
         if movie_name is None:
             movie_name = cleanString(response.css("div.title_wrapper h1::text").get())
 
+        rating_value = response.css("div.ratingValue strong span::text").get()
+
         movie_year = cleanString(
                     response.css("div.subpage_title_block span.nobr::text").get()
         )
@@ -23,30 +37,34 @@ class ActorsInfoSpider(scrapy.Spider):
             movie_year = response.css("div.title_wrapper h1 span a::text").get()
 
         movie_id = None
+        if movie_year is not None and rating_value is not None:
+            for meta in response.css("meta"):
+                try: #not all the meta tags have attribute property
+                    prop = meta.attrib['property']
+                    if prop == "pageId":
+                        movie_id = meta.attrib['content']
+                except:
+                    pass
 
-        for meta in response.css("meta"):
-            try: #not all the meta tags have attribute property
-                prop = meta.attrib['property']
-                if prop == "pageId":
-                    movie_id = meta.attrib['content']
-            except:
-                pass
-
-        for tr in response.css("table.cast_list tr"):
-            actor_name = cleanString(tr.css("a::text").get())
-            if actor_name is not None and actor_name is not "": #some actor names are ""
-                role_name = cleanString(tr.css("td.character a::text").get())
-                if role_name is None or role_name is "": #sometimes role_name is ""
-                    role_name = cleanString(tr.css("td.character::text").get())
-                actor_id = cleanString(tr.css("td.primary_photo a::attr(href)").get()).split('/')[2]
-                yield{
-                    'movie_id': movie_id,
-                    'movie_name': movie_name,
-                    'movie_year': movie_year,
-                    'actor_name': actor_name,
-                    'role_name': role_name,
-                    'actor_id': actor_id
-                }
+            for tr in response.css("table.cast_list tr"):
+                actor_name = cleanString(tr.css("a::text").get())
+                if actor_name is not None and actor_name is not "": #some actor names are ""
+                    role_name = cleanString(tr.css("td.character a::text").get())
+                    if role_name is None or role_name is "": #sometimes role_name is ""
+                        role_name = cleanString(tr.css("td.character::text").get())
+                    actor_id = cleanString(tr.css("td.primary_photo a::attr(href)").get()).split('/')[2]
+                    es.index(index='imdb',
+                             doc_type='movies',
+                             id=uuid.uuid4(),
+                             body={
+                                    'movie_id': movie_id,
+                                    'movie_name': movie_name,
+                                    'movie_year': int(movie_year),
+                                    'movie_rating': float(rating_value),
+                                    'actor_name': actor_name,
+                                    'role_name': role_name,
+                                    'actor_id': actor_id
+                    })
         actor_page = response.css('table.cast_list tr td.primary_photo a::attr(href)').get()
         if actor_page is not None:
             actor_page = response.urljoin(actor_page)
